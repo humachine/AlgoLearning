1782c06f3b979da037fe273ef66d5be7270fcfb4
Humachine <swarun@gmail.com>
2016-04-13 00:07:35 -0700

    Completed Kadanes Algorithm

--- Arrays/KadanesAlgm.cpp
+++ Arrays/KadanesAlgm.cpp
@@ -1,6 +1,9 @@
 //http://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 //
 //Find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum. 
+//
+//Time Complexity = O(n)
+//Space Complexity = O(1)
 
 #include<iostream>
 #include<limits.h>
@@ -10,7 +13,7 @@ int main()
 {
     int N;
     cin>>N;
-    int i, j, k, sum, ans;
+    int i, j, k, sum, ans, arr[1000];
     int max;
     cin>>max;
     int allneg=1;
@@ -22,15 +25,21 @@ int main()
         if(arr[i]>0)
             allneg=0;
     }
-
     if(allneg)
         cout<<max<<endl;
     else{
-        ans=arr[0];
-        for(i=1;i<N;i++){
-            if(arr[i]>
+        int maxsofar, maxtillhere;
+        maxsofar = maxtillhere = 0;
+        for(i=0;i<N;i++){
+            maxtillhere = maxtillhere + arr[i];
+            if(maxtillhere < 0)
+                maxtillhere = 0;
+            if(maxsofar < maxtillhere)
+                maxsofar = maxtillhere;
         }
+        cout<<maxsofar<<endl;
     }
+
     return 0;
 }
 
