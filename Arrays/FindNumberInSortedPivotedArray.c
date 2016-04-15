a90c078528de6543f7512d7df9a50cbb18b1f161
Humachine <swarun@gmail.com>
2016-04-15 11:30:20 -0700

    Started the Pivoted Sorted Array Search

---
+++ Arrays/FindNumberInSortedPivotedArray.c
@@ -0,0 +1,20 @@
+//http://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+//
+//While O(logn) sort is easy in a sorted array, think of a sorted pivoted array : 3 4 5 1 2
+//Now try to find a given number in O(logn) with O(1) space
+//
+
+#include<stdio.h>
+int main()
+{
+    int arr[100];
+    int i, j, k, N;
+    int ans;
+    int left, right, mid;
+
+    scanf("%d", &N);
+    for(i=0;i<N;i++){
+        scanf("%d", &arr[i]);
+    }
+    return 0;
+}
