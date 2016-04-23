949b62d8c18ca0a24a259ab6d054552b9b4b07bd
Humachine <swarun@gmail.com>
2016-04-22 18:05:08 -0700

    Started median of sorted arrays problem

---
+++ Arrays/MedianOfSortedArrays.c
@@ -0,0 +1,14 @@
+//http://www.geeksforgeeks.org/median-of-two-sorted-arrays/
+//
+//Given two sorted arrays of equal size, find the median of the two arrays
+
+#include<stdio.h>
+int main()
+{
+    int a[] = {1, 3, 4, 8, 9};
+    int b[] = {2, 5, 17, 35, 77};
+
+    int i, j, k;
+    return 0;
+}
+
