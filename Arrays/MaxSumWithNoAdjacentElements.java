d4225bd44643597c6a35980f9fdb86dd27393291
humachine <swarun@gmail.com>
2016-04-25 16:45:19 -0700

    Indonas AutoCommit Script + MaxSum DP Problem

---
+++ Arrays/MaxSumWithNoAdjacentElements.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class MaxSumWithNoAdjacentElements{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int i, j, k;
+    }
+}
