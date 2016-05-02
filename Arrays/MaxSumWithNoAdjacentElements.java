f88b225763b3e22911c4d11cc1a637ed150d094d
humachine <swarun@gmail.com>
2016-05-02 16:42:18 -0700

    Added Maximum Sub with no adjacent elements code - Recursive Solution

--- Arrays/MaxSumWithNoAdjacentElements.java
+++ Arrays/MaxSumWithNoAdjacentElements.java
@@ -1,4 +1,6 @@
+//http://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
 import java.util.Scanner;
+import java.lang.*;
 class MaxSumWithNoAdjacentElements{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -6,5 +8,22 @@ class MaxSumWithNoAdjacentElements{
         int N = in.nextInt();
 
         int i, j, k;
+        int[] arr = new int[100];
+        for(i=0;i<N;i++)
+            arr[i] = in.nextInt();
+        for(i=0;i<N;i++)
+            System.out.println(arr[i]);
+        int ans = maxSum(arr, 0, N);
+        System.out.println("Ans is " + ans);
+    }
+    static int[] memo = new int[1000];
+    static int maxSum(int[] arr, int left, int right){
+        if(memo[left]>0)
+            return memo[left];
+        if(left<right){
+            memo[left] = Math.max(arr[left]+maxSum(arr, left+2, right), maxSum(arr, left+1, right));
+            return memo[left];
+        }
+        return 0;
     }
 }
