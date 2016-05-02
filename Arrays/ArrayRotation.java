68e765c6cc4072b8fd0e8423ae1dea87499bd676
humachine <swarun@gmail.com>
2016-05-01 22:16:01 -0700

    Rotate an Array by d steps

---
+++ Arrays/ArrayRotation.java
@@ -0,0 +1,15 @@
+//http://www.geeksforgeeks.org/array-rotation/
+//
+//Given an array, rotate the array. (i.e imagine array as a circular list and push list forward 'd' steps. 
+//Eg with d=2 and arr={1,2,3,4,5}, soln={4, 5, 1, 2, 3}
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class ArrayRotation {
+    public static void main(String[] args){
+        int[] array= {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int d = 3;
+    }
+}
+
--- Arrays/MedianOfSortedArrays.c
+++ Arrays/MedianOfSortedArrays.c
@@ -8,7 +8,9 @@ int main()
     int a[] = {1, 3, 4, 8, 9};
     int b[] = {2, 5, 17, 35, 77};
 
+    int med1, med2;
+
+
     int i, j, k;
     return 0;
 }
-
