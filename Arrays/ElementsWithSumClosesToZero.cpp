3fdb7121920dabf7f273d4d2bdf4330b1932cfba
humachine <swarun@gmail.com>
2016-05-12 02:10:53 -0700

    Using Vectors for the first time

---
+++ Arrays/ElementsWithSumClosesToZero.cpp
@@ -0,0 +1,18 @@
+//http://www.geeksforgeeks.org/two-elements-whose-sum-is-closest-to-zero/
+//
+//Given an array of elements, find the 2 elements whose sum is closest to zero
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int arr[] ={1, 60, -10, 70, 80, 85};
+    vector<int> vec(arr, arr+sizeof(arr)/sizeof(arr[0]));
+    
+
+    return 0;
+}
+
