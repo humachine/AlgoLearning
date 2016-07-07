8dd21351b99b7b7679f0491a3ba572a435bd543a
humachine <swarun@gmail.com>
2016-07-06 21:22:12 -0700

    The Grind begins. . .

---
+++ TheGrind/1Week1/BinarySearch.cpp
@@ -0,0 +1,25 @@
+// http://quiz.geeksforgeeks.org/binary-search/
+//Given a sorted array arr[] of n elements, write a function to search a given element x in arr[].
+//
+// Input: Array of N numbers
+// Output: -1 if number doesn't exist, position of number if it exists
+// Complexity : O(logN)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+
+    int i, j, k;
+    for(i=0;i<T;i++){
+        cin>>N;
+
+        for(j=0;j<N;j++){
+        }
+    }
+
+    return 0;
+}
