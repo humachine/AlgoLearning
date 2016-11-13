96227f398cad4490ad16fe2b5bc61630cbdfd9a5
Humachine <swarun@gmail.com>
2016-11-13 15:43:15 -0800

    Pairing strings

--- leetcode/Done/336_PalindromePairs.cpp
+++ leetcode/Done/336_PalindromePairs.cpp
@@ -6,95 +6,63 @@
 #include<algorithm>
 using namespace std;
 class Solution{
-        void LoadReverseWords(const vector<string>& words, unordered_map<string, int>& pal_map){
-            int i=0;
-            for(i=0;i<words.size();i++){
-                string word(words[i].rbegin(), words[i].rend());
-                pal_map.insert(make_pair(word, i));
-            }
+    void LoadReverseWordMap(vector<string>& words, unordered_map<string, int>& palMap){
+        for(int i=0;i<words.size();i++){
+            string temp(words[i].rbegin(), words[i].rend());
+            palMap[temp] = i+1;
         }
-        bool isPalindrome(string s){
-            if(s.empty() || s.size()==1)   return true;
-            int i, j;
-            for(i=0, j=s.size()-1; i<j;i++, j--){
-                if(s[i] != s[j])
-                    return false;
-            }
-            return true;
+    }
+    bool isPalindrome(string s){
+        if(s.empty())   return true;
+        int i, n=s.size();
+        for(int i=0; i<n/2; i++){
+            if(s[i] != s[n-i-1])
+                return false;
         }
-        void addPalindromes(const vector<string>& words,
-                const unordered_map<string, int>& pal_map, vector<vector<int>>& answer){
-            int i=0, emptyStrLoc;
-            auto it = pal_map.find("");
-            emptyStrLoc = it->second;
-
-            for(i=0;i<words.size();i++){
-                string word = words[i];
-                if(word == "")  continue;
-                if(isPalindrome(word)){
-                    vector<int> v(2, 0);
-                    v[0] = emptyStrLoc; v[1]=i;
-                    answer.push_back(v);
-                    v[0] = i; v[1]=emptyStrLoc;
-                    answer.push_back(v);
-                }
-            }
-        }
+        return true;
+    }
     public:
         vector<vector<int>> palindromePairs(vector<string>& words) {
-            vector<vector<int>> answer;
-            if(words.empty()){
-                return answer;
-            }
-            if(words.size()==1){
-                answer.push_back(vector<int>());
-                return answer;
-            }
-            unordered_map<string, int> pal_map;
-            LoadReverseWords(words, pal_map);
-            
-            //For each word
-            int i, j, left, mid, right;
-            bool has_empty = false;
+            //Sanitize
+            vector<vector<int>> result;
+            if(words.empty())
+               return result; 
+            //Create Reverse Word Dictionary
+            unordered_map<string, int> palMap;
+            LoadReverseWordMap(words, palMap);
+
+            //For each word in words
+            //if words[0...i] is a palindrome, check if words[j...end] has its reverse in palMap
+            //if words[i...end] is a palindrome, check if words[0...i] has its reverse in palMap
+            int i, j, k;
+            string left, right;
             vector<int> v(2, 0);
             for(i=0; i<words.size(); i++){
-                string word = words[i];
-                for(j=0; j<word.size(); j++){
-                    string leftw = word.substr(0, j);
-                    string rightw = word.substr(j, word.size()-j);
-                    if(isPalindrome(leftw) && rightw!=""){
-                        auto it = pal_map.find(rightw);
-                        if(it!=pal_map.end()){
-                            v[0] = it->second;
+                for(j=0; j<=words[i].size();j++){
+                    left = words[i].substr(0, j);
+                    right = words[i].substr(j);
+                    if(isPalindrome(left) && palMap[right] > 0 && palMap[right]-1 != i){
+                            v[0] = palMap[right]-1;
                             v[1] = i;
-                            if(i != it->second)
-                                answer.push_back(v);
-                        }
+                            result.push_back(v);
                     }
-                    if(isPalindrome(rightw) && leftw!=""){
-                        auto it = pal_map.find(leftw);
-                        if(it!=pal_map.end()){
+                    if(isPalindrome(right) && right.size()>0 && palMap[left] > 0 && palMap[left]-1 != i){
                             v[0] = i;
-                            v[1] = it->second;
-                            if(i != it->second)
-                                answer.push_back(v);
-                        }
+                            v[1] = palMap[left] - 1;
+                            result.push_back(v);
                     }
                 }
-                if(word == "")
-                    has_empty = true;
             }
-            if(has_empty)
-                addPalindromes(words, pal_map, answer);
-            return answer;
+            return result;
         }
 };
 int main(){
     //string w[] = {"bat", "tab", "cat"};
-    string w[] = {"abcd", "dcba", "lls", "s", "sssll"};
-    vector<string> words(w, w+5);
+    string w[] = {"abcd", "dcba", "lls", "s", "sssll", ""};
+    vector<string> words(w, w+6);
     class Solution s;
     vector<vector<int>> answer = s.palindromePairs(words);
+    cout<<endl<<endl;
     for(int i=0;i<answer.size();i++)
         cout<<words[answer[i][0]]<<' '<<words[answer[i][1]]<<endl;
 }
