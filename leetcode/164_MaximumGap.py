9a7faa42c48e2ad680836e258cb0e6a7716dcf37
Humachine <swarun@gmail.com>
2016-12-21 14:16:46 -0800

    Maximum Gap Problem

---
+++ leetcode/164_MaximumGap.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/maximum-gap
+'''
+Given an unsorted array, find the maximum gap between consecutive elements in the sorted version of the array
+'''
+class Solution(object):
+    def maximumGap(self, nums):
+        if len(nums)<2: return 0
+
+        minimum, maximum, n = min(nums), max(nums), len(nums)
+        average = sum(nums)/n
+        numberMap = {nums[i]:i for i in xrange(n)}
+        if minimum == maximum:
+            return 0
+        maxGap = 1
+        start = minimum
+        while start < maximum:
+            nextElem = 
+            # Find next elem, update maxGap
