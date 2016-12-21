8cbc2729d0bea46418550be61871e49ae6ec5254
Humachine <swarun@gmail.com>
2016-12-20 18:24:16 -0800

    Added 2 pass Connected components based solution

--- leetcode/130_SurroundedRegions.py
+++ leetcode/130_SurroundedRegions.py
@@ -12,16 +12,8 @@ X X X X
 X X X X
 X O X X
 '''
+from Queue import Queue
 class Solution(object):
-    def dfs(self, board, i, j):
-        m, n = len(board), len(board[0])
-        board[i][j] = 'o'
-
-        if i<m-1 and board[i+1][j]=='O': self.dfs(board, i+1, j)
-        if i>0 and board[i-1][j]=='O':   self.dfs(board, i-1, j)
-        if j>0 and board[i][j-1]=='O':   self.dfs(board, i, j-1)
-        if j<n-1 and board[i][j+1]=='O': self.dfs(board, i, j+1)
-
     def printBoard(self, board):
         m, n = len(board), len(board[0])
         for i in xrange(m):
@@ -29,28 +21,38 @@ class Solution(object):
                 print board[i][j], 
             print
 
+    def floodBoard(self, i, j, board):
+        m, n = len(board), len(board[0])
+        cells = Queue()
+        cells.put((i, j))
+        while not cells.empty():
+            x, y = cells.get()
+            board[x][y] = 'o'
+            if x>0 and board[x-1][y]=='O': cells.put((x-1, y))
+            if x<m-1 and board[x+1][y]=='O': cells.put((x+1, y))
+            if y>0 and board[x][y-1]=='O': cells.put((x, y-1))
+            if y<n-1 and board[x][y+1]=='O': cells.put((x, y+1))
+
     def solve(self, board):
         if not board:   return
         m, n = len(board), len(board[0])
+        if m<=2 or n<=2:    return
 
         for i in [0, m-1]:
             for j in xrange(n):
                 if board[i][j] == 'O':
-                    print i, j
-                    self.dfs(board, i, j)
-        for j in [0, n-1]:
-            for i in xrange(m):
+                    self.floodBoard(i, j, board)
+        for i in xrange(m):
+            for j in [0, n-1]:
                 if board[i][j] == 'O':
-                    print i, j
-                    self.dfs(board, i, j)
+                    self.floodBoard(i, j, board)
+
         for i in xrange(m):
             for j in xrange(n):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'
-                elif board[i][j] == 'o':
-                    board[i][j] = 'O'
+                board[i][j] = 'XO'[board[i][j]=='o']
         self.printBoard(board)
 
 board = ['X X X X'.split(), 'X O O X'.split(), 'X X O X'.split(), 'X O X X'.split()]
 s = Solution()
 s.solve(board)
+
