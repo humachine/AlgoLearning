5c2928fbe55cc3a4214d4dc52c8500976ea4a469
Humachine <swarun@gmail.com>
2017-01-23 07:32:33 -0800

    Longest Substring using sets

--- leetcode/Done/3_LongestSubstrNoRepeatingChars.py
+++ leetcode/Done/3_LongestSubstrNoRepeatingChars.py
@@ -36,8 +36,32 @@ class Solution(object):
         #maxString = s[maxStart:maxEnd+1]
         return maxLen
 
+    def lengthOfLongestSubstring(self, s):
+        ''' This is a solution similar to the above one. Here, we maintain a set of all
+        the characters that go into our valid non-repeating string. 
+        Every time we see a duplicate character, we update our string to make it valid again.
+        '''
+        charSet = set()
+        maxLen = start = end = 0
+        while end < len(s):
+            char = s[end]
+            # If the current character has not been seen previously
+            if char not in charSet:
+                charSet.add(char)
+            else:
+                # Discard all characters upto and including the previous occurence of $char
+                while s[start] != char:
+                    charSet.discard(s[start])
+                    start+=1
+                start+=1
+            # Update max length of string if necessary
+            maxLen = max(end-start+1, maxLen)
+            end+=1
+        return maxLen
+
 s = Solution()
 print s.lengthOfLongestSubstring('abcabcbb')
 print s.lengthOfLongestSubstring('bbbb')
 print s.lengthOfLongestSubstring('pwwkew')
 print s.lengthOfLongestSubstring('abba')
+print s.lengthOfLongestSubstring("tmmzuxt")
