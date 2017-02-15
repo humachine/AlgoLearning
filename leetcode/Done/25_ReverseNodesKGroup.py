a1760918d38eb6644dbc7f733aecbadb76a004d0
Humachine <swarun@gmail.com>
2017-02-15 10:17:39 -0800

    Reverse Nodes K Group

--- leetcode/Done/25_ReverseNodesKGroup.py
+++ leetcode/Done/25_ReverseNodesKGroup.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/reverse-nodes-in-k-group/
+#https://leetcode.com/problems/reverse-nodes-in-k-groupx/
 ''' Given a set of nodes, reverse them in groups of k.
 If k does not divide the number of nodes, then the remainder nodes at the end of the list are left unreversed.
 '''
@@ -8,9 +8,10 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
-    def _reverseSegment(self, start, end):
-        ''' This is the function that reverses a sequence of nodes starting AFTER start and ending BEFORE end.'''
-        prev, curr = start.next, start.next.next
+    def _reverseSegment(self, prev, end):
+        ''' This is the function that reverses a sequence of nodes preving AFTER prev and ending BEFORE end.'''
+        pprev = prev
+        prev, curr = prev.next, prev.next.next
         while curr and curr!=end:
             # We perform the reversal such:
             # curr.next is stored in a temp variable. 
@@ -19,26 +20,25 @@ class Solution(object):
             # This can be written as below in a single line because python Tuple assignements are performed after evaluating all the RHS elements before assignements. Also, the tuple assignements are performed variable by variable L-R
             # This means, curr.next is first assigned to prev, curr to the already evaluated value of curr.next (not prev) and prev to the already evaluated value of curr (not curr.next)
             curr.next, curr, prev = prev, curr.next, curr
+        # node = pprev
+        # while node!=end:
+            # print node.val
+            # node = node.next
+        print 'attaching', pprev.next.val, '->', end.val
+        next_start = pprev.next
+        pprev.next.next = end
+        print 'attaching', pprev.val, '->', prev.val
+        pprev.next = prev
+        node = pprev
+        while node:
+            print node.val, '->',
+            node = node.next
+        print
+        print next_start.val, next_start.next.val
 
         # nextStart denotes the location after which the NEXT k-group begins
-        nextStart, nextStart.next, start.next = start.next, end, prev
-        return nextStart
-
-    def _reverseKGroup(self, prev, k):
-        ''' This is a function used to reverse a k-Segment of the linked list.
-        Firstly, we start at prev.next and see if there are at least k-1 more nodes in the linked list. 
-        If there are not, then we have reached the end of the linked list and hence we just return.
-        
-        If we have k-nodes, we reverse the k-segment and obtain the last node of the REVERSED sequence.
-        That last node becomes the prev(or previous/parent) of the next k-group, if any'''
-        fast = prev.next
-        for i in xrange(k):
-            if not fast:
-                return
-            fast = fast.next
-
-        newStart = self._reverseSegment(prev, fast)
-        self._reverseKGroup(newStart, k)
+        # nextStart, nextStart.next, prev.next = prev.next, end, prev
+        return next_start
 
 
     def reverseKGroup(self, head, k):
@@ -46,12 +46,22 @@ class Solution(object):
         This is performed in a 3-step process:
         i) Find the next k-group to reverse
         ii) Reverse the nodes of the k-group (if any)
-        iii) Attach the revrsed k-group back into the original linked list appropriately.
+        iii) Attach the reversed k-group back into the original linked list appropriately.
         '''
-        if not head or k==1:    return head
         temphead = ListNode(-1)
         temphead.next = head
-        self._reverseKGroup(temphead, k)
+
+        prev = temphead
+        curr = prev.next
+        while curr:
+            for i in xrange(k):
+                if not curr:
+                    #If there are less than K nodes in the current group, break and return
+                    return temphead.next
+                curr = curr.next
+    
+            newStart = self._reverseSegment(prev, curr)
+            prev, curr = newStart, newStart.next
         return temphead.next
 
 head = ListNode(1)
@@ -66,3 +76,4 @@ res = s.reverseKGroup(head, 3)
 while res:
     print res.val, '->',
     res = res.next
+     
